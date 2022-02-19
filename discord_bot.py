ff23acedbae712285e985ea6d56d49a21078940f
brandonfrulla <bfrulla@mail.umw.edu>
2022-02-19 15:13:25 -0500

    modified:   discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -48,16 +48,16 @@ async def on_message(message):
         if (len(_list)<2):
             await message.channel.send("Include a slug ticker!\n\nTo ask for help, call '/f help'")
         else:
-            if(_list[1] == 'help'):
+            if(_list[1].lower() == 'help'):
                 reply = format("Acceptable command slugs: \n\n{slugs}".format(slugs = list(slugs.keys())))
                 await message.channel.send(reply)
-            elif (_list[1] in slugs.keys()):
+            elif (_list[1].lower() in slugs.keys()):
                 newUrl = url + slugs.get(_list[1]) 
                 response = requests.request("GET", newUrl)
-                json = response.json()
-                collection = json['collection']
-                stats = collection['stats']
-                floor = stats['floor_price']
+                floor = response.json()['collection']['stats']['floor_price']
+                # collection = json['collection']
+                # stats = collection['stats']
+                # floor = stats['floor_price']
                 reply = format("{slug} Floor: {fl}".format(slug = str.upper(_list[1]), fl = floor))
                 await message.channel.send(reply)
             else:
