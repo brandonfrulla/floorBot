c9ea1f7dc0e01d6c63226d9b2bb19f8e63aa8a01
brandonfrulla <bfrulla@mail.umw.edu>
2022-02-19 15:27:25 -0500

    discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -50,16 +50,16 @@ async def on_message(message):
 
     if message.content.startswith('/f'):
 
-        slug = message.content
+        slug = message.content.lower()
         _list = slug.split(" ")
 
         if (len(_list)<2):
             await message.channel.send("Include a slug ticker!\n\nTo ask for help, call '/f help'")
         else:
-            if(_list[1].lower() == 'help'):
+            if(_list[1] == 'help'):
                 reply = format("Acceptable command slugs: \n\n{slugs}".format(slugs = list(slugs.keys())))
                 await message.channel.send(reply)
-            elif (_list[1].lower() in slugs.keys()):
+            elif (_list[1] in slugs.keys()):
                 newUrl = url + slugs.get(_list[1]) 
                 floor = requests.request("GET", newUrl).json()['collection']['stats']['floor_price']
                 reply = format("{slug} Floor: {fl}".format(slug = str.upper(_list[1]), fl = floor))
