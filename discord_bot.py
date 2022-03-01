d4d84302a77f4da2301f76d48e94b258e2e748ec
brandonfrulla <bfrulla@mail.umw.edu>
2022-03-01 11:22:17 -0500

    added verbose seting to bot

--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,7 @@ File_object = open(".botPw", "r")
 
 url = "https://api.opensea.io/api/v1/collection/"
 slugs = { 
-            'w3' : "worldwidewebbland",
+            'w3' : "worldwidewebbland?search[sortAscending]=true&search[sortBy]=PRICE&search[stringTraits][0][name]=type&search[stringTraits][0][values][0]=large%20apartment",
             'galaxyfc' : "galaxy-fight-club",
             'hgalaxy': "hero-galaxy-heroes",
             'horde': "edenhorde-official",
@@ -57,15 +57,31 @@ async def on_message(message):
         if (len(_list)<2):
             await message.channel.send("Include a slug ticker!\n\nTo ask for help, call '/f help'")
         else:
-            if(_list[1] == 'help'):
-                reply = format("Acceptable command slugs: \n\n{slugs}".format(slugs = list(slugs.keys())))
-                await message.channel.send(reply)
-            elif (_list[1] in slugs.keys()):
+            if (len(_list) == 2):
+                if(_list[1] == 'help'):
+                    reply = format("Acceptable command slugs: \n\n{slugs}".format(slugs = list(slugs.keys())))
+                    await message.channel.send(reply)
+                elif (_list[1] in slugs.keys()):
+                    newUrl = url + slugs.get(_list[1]) 
+                    floor = requests.request("GET", newUrl).json()['collection']['stats']['floor_price']
+                    reply = format("{slug} Floor: {fl}".format(slug = str.upper(_list[1]), fl = floor))
+                    await message.channel.send(reply)
+                else:
+                    await message.channel.send(format("I don't know that slug yet, add it to my dictionary via pull request here! \n{link}".format(link = 'https://github.com/brandonfrulla/floorBot/blob/main/discord_bot.py')))
+            elif ((len(_list) == 3) and (str.lower(_list[2]) == "v")):
                 newUrl = url + slugs.get(_list[1]) 
-                floor = requests.request("GET", newUrl).json()['collection']['stats']['floor_price']
-                reply = format("{slug} Floor: {fl}".format(slug = str.upper(_list[1]), fl = floor))
+                data = requests.request("GET", newUrl).json()['collection']['stats']
+                floor = data['floor_price']
+                _1dS = data['one_day_sales']
+                _1dV = data['one_day_volume']
+                _1dC = data['one_day_change']
+                _7dS = data['seven_day_sales']
+                _7dV = data['seven_day_volume']
+                _7dC = data['seven_day_change']
+                _30dS = data['thirty_day_sales']
+                _30dV = data['thirty_day_volume']
+                _30dC = data['thirty_day_change']
+                reply = format("{slug} Floor: {fl}\n*\nOne Day Sales: {_1ds}\nOne Day Volume: {_1dv}\nOne Day Change: {_1dc}\n*\nSeven Day Sales: {_7ds}\nSeven Day Volume: {_7dv}\nSeven Day Change: {_7dc}\n*\nThirty Day Sales: {_30ds}\nThirty Day Volume: {_30dv}\nThirty Day Change: {_30dc}".format(slug = str.upper(_list[1]), fl = floor, _1ds = _1dS, _1dv = _1dV ,_1dc = _1dC, _7ds = _7dS, _7dv = _7dV, _7dc = _7dC, _30ds = _30dS, _30dv = _30dV, _30dc = _30dC))
                 await message.channel.send(reply)
-            else:
-                await message.channel.send(format("I don't know that slug yet, add it to my dictionary via pull request here! \n{link}".format(link = 'https://github.com/brandonfrulla/floorBot/blob/main/discord_bot.py')))
 
 client.run(File_object.read())
